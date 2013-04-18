636538ec3bc9df6371a5461b53eb3b7d02936b20
irish <irish@irish-desctop.(none)>
2013-04-19 00:55:27 +0300

    certificates, landing page

--- sites/all/themes/geekhub/templates/page.tpl.php
+++ sites/all/themes/geekhub/templates/page.tpl.php
@@ -161,11 +161,17 @@
       <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title">
-          <?php print $title; ?>
-        </h1>
-      <?php endif; ?>
+
+        <?php if (!$is_front) { ?>
+        <?php if ($title) { ?>
+            <h1><?php print $title ?></h1>
+            <?php }
+    } ?>
+<!--      --><?php //if ($title): ?>
+<!--        <h1 class="title" id="page-title">-->
+<!--          --><?php //print $title; ?>
+<!--        </h1>-->
+<!--      --><?php //endif; ?>
       <?php print render($title_suffix); ?>
       <?php if ($tabs): ?>
         <div class="tabs">
